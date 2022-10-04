3e694d50368fdf0ec1ee209c388770e8e8224554
Luc <8822552+luc-github@users.noreply.github.com>
2022-10-04 16:33:03 +0800

    Add littlefs component for flash

--- components/littlefs/component.mk
+++ components/littlefs/component.mk
@@ -1,4 +1,14 @@
-# Generic component
+#
+# Component Makefile
+#
 
-COMPONENT_SRCDIRS := . 
-COMPONENT_ADD_INCLUDEDIRS := .
+COMPONENT_SRCDIRS := src src/littlefs
+
+COMPONENT_ADD_INCLUDEDIRS := include
+
+COMPONENT_PRIV_INCLUDEDIRS := src
+
+COMPONENT_SUBMODULES := src/littlefs
+
+CFLAGS += \
+	-DLFS_CONFIG=lfs_config.h
