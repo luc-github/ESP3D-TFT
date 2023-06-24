066752cc0d82f21d675bbc137839e7431a7d034f
Luc <8822552+luc-github@users.noreply.github.com>
2023-06-24 20:10:24 +0800

    Fix typo

--- hardware/ESP32_ROTRICS_DEXARM35/components/bsp/lv_conf.h
+++ hardware/ESP32_ROTRICS_DEXARM35/components/bsp/lv_conf.h
@@ -388,7 +388,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_FONT_CUSTOM_DECLARE
 
 /*Always set a default font*/
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &lv_font_montserrat_20
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
