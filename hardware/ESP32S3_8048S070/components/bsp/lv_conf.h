d16e6b519b654123dd9273a1451222071795d19c
Luc <8822552+luc-github@users.noreply.github.com>
2023-07-23 07:36:21 +0800

    Update LV_TICK_CUSTOM routine

--- hardware/ESP32S3_8048S070/components/bsp/lv_conf.h
+++ hardware/ESP32S3_8048S070/components/bsp/lv_conf.h
@@ -87,8 +87,8 @@
  *It removes the need to manually update the tick with `lv_tick_inc()`)*/
 #define LV_TICK_CUSTOM 0
 #if LV_TICK_CUSTOM
-#define LV_TICK_CUSTOM_INCLUDE "Arduino.h"         /*Header for the system time function*/
-#define LV_TICK_CUSTOM_SYS_TIME_EXPR (millis())    /*Expression evaluating to current system time in ms*/
+    #define LV_TICK_CUSTOM_INCLUDE "esp_timer.h"         /*Header for the system time function*/
+    #define LV_TICK_CUSTOM_SYS_TIME_EXPR (esp_timer_get_time() / 1000LL)    /*Expression evaluating to current system time in ms*/
 #endif   /*LV_TICK_CUSTOM*/
 
 /*Default Dot Per Inch. Used to initialize default sizes such as widgets sized, style paddings.
