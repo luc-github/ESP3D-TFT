28392e358d9ee864b9c56258f863d02ff66b19ad
Luc <8822552+luc-github@users.noreply.github.com>
2023-09-04 14:40:23 +0800

    Manual bed leveling basic UI -wip

--- main/display/3dprinter/marlin/screens/auto_leveling_screen.cpp
+++ main/display/3dprinter/marlin/screens/auto_leveling_screen.cpp
@@ -22,6 +22,7 @@
 
 #include <lvgl.h>
 
+#include "components/back_button_component.h"
 #include "esp3d_log.h"
 #include "esp3d_styles.h"
 #include "esp3d_tft_ui.h"
@@ -31,23 +32,42 @@
  *  STATIC PROTOTYPES
  **********************/
 namespace autoLevelingScreen {
-void event_button_handler(lv_event_t *e) {
-  esp3d_log("back Clicked");
+lv_timer_t *auto_leveling_screen_delay_timer = NULL;
+
+void auto_leveling_screen_delay_timer_cb(lv_timer_t *timer) {
+  if (auto_leveling_screen_delay_timer) {
+    lv_timer_del(auto_leveling_screen_delay_timer);
+    auto_leveling_screen_delay_timer = NULL;
+  }
   levelingScreen::leveling_screen(true);
+}
+
+void event_button_fan_back_handler(lv_event_t *e) {
+  esp3d_log("back Clicked");
+  if (BUTTON_ANIMATION_DELAY) {
+    if (auto_leveling_screen_delay_timer) return;
+    auto_leveling_screen_delay_timer = lv_timer_create(
+        auto_leveling_screen_delay_timer_cb, BUTTON_ANIMATION_DELAY, NULL);
+  } else {
+    auto_leveling_screen_delay_timer_cb(NULL);
+  }
 }
 
 void auto_leveling_screen() {
   esp3dTftui.set_current_screen(ESP3DScreenType::none);
   // Screen creation
-  esp3d_log("Main screen creation");
+  esp3d_log("Auto leveling screen creation");
   lv_obj_t *ui_new_screen = lv_obj_create(NULL);
   // Display new screen and delete old one
   lv_obj_t *ui_current_screen = lv_scr_act();
   lv_scr_load(ui_new_screen);
   lv_obj_del(ui_current_screen);
   apply_style(ui_new_screen, ESP3DStyleType::main_bg);
-  lv_obj_add_event_cb(ui_new_screen, event_button_handler, LV_EVENT_CLICKED,
+
+  lv_obj_t *btnback = backButton::create_back_button(ui_new_screen);
+  lv_obj_add_event_cb(btnback, event_button_fan_back_handler, LV_EVENT_CLICKED,
                       NULL);
+
   esp3dTftui.set_current_screen(ESP3DScreenType::auto_leveling);
 }
 
