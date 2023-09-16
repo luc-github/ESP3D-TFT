8fd40cfcbd54b51caa28a042224290feea0ffd49
Luc <8822552+luc-github@users.noreply.github.com>
2023-09-17 07:45:32 +0800

    Update fw_simulator.py

--- tools/fw_simulator/fw_simulator.py
+++ tools/fw_simulator/fw_simulator.py
@@ -7,7 +7,7 @@ import random
 
 
 class bcolors:
-    HEADER = '\033[95m'
+    COL_PURPLE = '\033[95m'
     COL_BLUE = '\033[94m'
     COL_CYAN = '\033[96m'
     COL_GREEN = '\033[92m'
@@ -77,7 +77,7 @@ def generateTemperatureResponse(withok):
 
 def send_echo(ser, msg):
     ser.write((msg + "\n").encode('utf-8'))
-    print(bcolors.END_COL + msg + bcolors.END_COL)
+    print(bcolors.HEADER + msg + bcolors.END_COL)
 
 
 def display_busy(ser, nb):
