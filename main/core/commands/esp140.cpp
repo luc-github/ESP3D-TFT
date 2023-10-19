06e76e22560b903da78c066287d14b1631fb8d0c
Luc <8822552+luc-github@users.noreply.github.com>
2023-10-19 16:11:43 +0800

    Update esp140.cpp

--- main/core/commands/esp140.cpp
+++ main/core/commands/esp140.cpp
@@ -30,176 +30,118 @@
 //[ESP140]<sync> <srv1=XXXXX> <srv2=XXXXX> <srv3=XXXXX> <tzone=xxx> <dst=YES/NO>
 //<time=YYYY-MM-DDTHH:mm:ss> <now> json=<no> pwd=<admin password>
 void ESP3DCommands::ESP140(int cmd_params_pos, ESP3DMessage* msg) {
-  /*  ESP3DClientType target = msg->origin;
-    ESP3DRequest requestId = msg->request_id;
-    (void)requestId;
-    msg->target = target;
-    msg->origin = ESP3DClientType::command;
-    bool hasError = false;
-    std::string error_msg = "Invalid parameters";
-    std::string ok_msg = "ok";
-    bool json = hasTag(msg, cmd_params_pos, "json");
-    bool sync = hasTag(msg, cmd_params_pos, "sync");
-    bool now = hasTag(msg, cmd_params_pos, "now");
-    std::string tmpstr;
-    const char* cmdList[] = {"srv1=", "srv2=", "srv3=", "tzone=", "time="};
-    uint8_t cmdListSize = sizeof(cmdList) / sizeof(char*);
-    const ESP3DSettingIndex settingIndex[] = {
-        ESP3DSettingIndex::esp3d_notification_type,
-        ESP3DSettingIndex::esp3d_notification_token_1,
-        ESP3DSettingIndex::esp3d_notification_token_2,
-        ESP3DSettingIndex::esp3d_notification_token_setting,
-        ESP3DSettingIndex::esp3d_auto_notification};
-  #if ESP3D_AUTHENTICATION_FEATURE
-    if (msg->authentication_level == ESP3DAuthenticationLevel::guest) {
-      msg->authentication_level = ESP3DAuthenticationLevel::not_authenticated;
-      dispatchAuthenticationError(msg, COMMAND_ID, json);
-      return;
-    }
-  #endif  // ESP3D_AUTHENTICATION_FEATURE
-    tmpstr = get_clean_param(msg, cmd_params_pos);
-    if (tmpstr.length() == 0) {
-      if (json) {
-        ok_msg = "{\"type\":\"";
-      } else {
-        ok_msg = "type: ";
-      }
-      uint8_t b =
-          esp3dTftsettings.readByte(ESP3DSettingIndex::esp3d_notification_type);
-      switch ((ESP3DNotificationType)b) {
-        case ESP3DNotificationType::none:
-          ok_msg += "NONE";
-          break;
-        case ESP3DNotificationType::pushover:
-          ok_msg += "PUSHOVER";
-          break;
-        case ESP3DNotificationType::email:
-          ok_msg += "EMAIL";
-          break;
-        case ESP3DNotificationType::line:
-          ok_msg += "LINE";
-          break;
-        case ESP3DNotificationType::telegram:
-          ok_msg += "TELEGRAM";
-          break;
-        case ESP3DNotificationType::ifttt:
-          ok_msg += "IFTTT";
-          break;
-        default:
-          ok_msg += "UNKNOWN";
-      }
-      if (json) {
-        ok_msg += "\",\"AUTO\":\"";
-      } else {
-        ok_msg += ", AUTO: ";
-      }
-
-      ok_msg += esp3dTftsettings.readByte(
-                    ESP3DSettingIndex::esp3d_auto_notification) == 1
-                    ? "YES"
-                    : "NO";
-      char buffer[SIZE_OF_SETTING_NOFIFICATION_TS + 1];
-      if (json) {
-        ok_msg += "\",\"TS\":\"";
-      } else {
-        ok_msg += ", TS: ";
-      }
-      ok_msg += esp3dTftsettings.readString(
-          ESP3DSettingIndex::esp3d_notification_token_setting, buffer,
-          SIZE_OF_SETTING_NOFIFICATION_TS);
-      if (json) {
-        ok_msg += "\"}";
-      } else {
-        ok_msg += "\n";
-      }
+  ESP3DClientType target = msg->origin;
+  ESP3DRequest requestId = msg->request_id;
+  // use the longuest string to avoid buffer overflow
+  char buffer[SIZE_OF_SERVER_URL + 1];
+  (void)requestId;
+  msg->target = target;
+  msg->origin = ESP3DClientType::command;
+  bool hasError = false;
+  std::string error_msg = "Invalid parameters";
+  std::string ok_msg = "ok";
+  bool json = hasTag(msg, cmd_params_pos, "json");
+  bool sync = hasTag(msg, cmd_params_pos, "sync");
+  bool now = hasTag(msg, cmd_params_pos, "now");
+  std::string tmpstr;
+  const char* cmdList[] = {"srv1", "srv2", "srv3", "tzone", "time"};
+  uint8_t cmdListSize = sizeof(cmdList) / sizeof(char*);
+  const ESP3DSettingIndex settingIndex[] = {
+      ESP3DSettingIndex::esp3d_time_server1,
+      ESP3DSettingIndex::esp3d_time_server2,
+      ESP3DSettingIndex::esp3d_time_server3,
+      ESP3DSettingIndex::esp3d_timezone,
+      ESP3DSettingIndex::unknown_index,
+  };
+#if ESP3D_AUTHENTICATION_FEATURE
+  if (msg->authentication_level == ESP3DAuthenticationLevel::guest) {
+    msg->authentication_level = ESP3DAuthenticationLevel::not_authenticated;
+    dispatchAuthenticationError(msg, COMMAND_ID, json);
+    return;
+  }
+#endif  // ESP3D_AUTHENTICATION_FEATURE
+  tmpstr = get_clean_param(msg, cmd_params_pos);
+  if (tmpstr.length() == 0) {
+    if (json) {
+      ok_msg = "{";
     } else {
-      bool hasParam = false;
-      uint8_t val;
-      for (uint8_t i = 0; i < cmdListSize; i++) {
-        tmpstr = get_param(msg, cmd_params_pos, cmdList[i]);
-        if (tmpstr.length() != 0) {
-          hasParam = true;
-          switch (settingIndex[i]) {
-            case ESP3DSettingIndex::esp3d_notification_type:
-              if (strcasecmp(tmpstr.c_str(), "NONE") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::none);
-              } else if (strcasecmp(tmpstr.c_str(), "PUSHOVER") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::pushover);
-              } else if (strcasecmp(tmpstr.c_str(), "LINE") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::line);
-              } else if (strcasecmp(tmpstr.c_str(), "EMAIL") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::email);
-              } else if (strcasecmp(tmpstr.c_str(), "IFTTT") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::ifttt);
-              } else if (strcasecmp(tmpstr.c_str(), "TELEGRAM") == 0) {
-                val = static_cast<uint8_t>(ESP3DNotificationType::telegram);
-              } else {
-                hasError = true;
-                error_msg = "Invalid type parameter";
-              }
-              if (!hasError) {
-                if (!esp3dTftsettings.writeByte(settingIndex[i], val)) {
-                  hasError = true;
-                  error_msg = "Set value failed";
-                }
-              }
-              break;
-            case ESP3DSettingIndex::esp3d_notification_token_1:
-            case ESP3DSettingIndex::esp3d_notification_token_2:
-            case ESP3DSettingIndex::esp3d_notification_token_setting:
-              if (esp3dTftsettings.isValidStringSetting(tmpstr.c_str(),
-                                                        settingIndex[i])) {
-                esp3d_log("Value %s is valid", tmpstr.c_str());
-                if (!esp3dTftsettings.writeString(settingIndex[i],
-                                                  tmpstr.c_str())) {
-                  hasError = true;
-                  error_msg = "Set value failed";
-                }
-              } else {
-                hasError = true;
-                error_msg = "Invalid token parameter";
-              }
-              break;
-            case ESP3DSettingIndex::esp3d_auto_notification:
-              if (strcasecmp(tmpstr.c_str(), "YES") == 0 ||
-                  strcasecmp(tmpstr.c_str(), "1") == 0 ||
-                  strcasecmp(tmpstr.c_str(), "ON") == 0 ||
-                  strcasecmp(tmpstr.c_str(), "ENABLED") == 0) {
-                val = 1;
-              } else if (strcasecmp(tmpstr.c_str(), "NO") == 0 ||
-                         strcasecmp(tmpstr.c_str(), "0") == 0 ||
-                         strcasecmp(tmpstr.c_str(), "OFF") == 0 ||
-                         strcasecmp(tmpstr.c_str(), "DISABLED") == 0) {
-                val = 0;
-              } else {
-                hasError = true;
-                error_msg = "Invalid auto parameter";
-              }
-              if (!hasError) {
-                if (!esp3dTftsettings.writeByte(settingIndex[i], val)) {
-                  hasError = true;
-                  error_msg = "Set value failed";
-                }
-              }
-              break;
-            default:
+      ok_msg = "";
+    }
+    // no need to show time right now
+    for (uint i = 0; i < cmdListSize - 1; i++) {
+      if (json) {
+        if (i > 0) {
+          ok_msg += ",";
+        }
+        ok_msg += "\"";
+      } else {
+        if (i > 0) {
+          ok_msg += ", ";
+        }
+      }
+      ok_msg += cmdList[i];
+
+      if (json) {
+        ok_msg += "\":\"";
+      } else {
+        ok_msg += ": ";
+      }
+      ok_msg += esp3dTftsettings.readString(settingIndex[i], buffer,
+                                            SIZE_OF_SERVER_URL);
+      if (json) {
+        ok_msg += "\"";
+      } else {
+      }
+    }
+    if (json) {
+      ok_msg += "\"}";
+    } else {
+      ok_msg += "\n";
+    }
+  } else {
+    bool hasParam = false;
+    std::string tmpkey;
+    for (uint8_t i = 0; i < cmdListSize; i++) {
+      tmpkey = cmdList[i];
+      tmpkey += "=";
+      tmpstr = get_param(msg, cmd_params_pos, tmpkey.c_str());
+      if (tmpstr.length() != 0) {
+        hasParam = true;
+        if (settingIndex[i] == ESP3DSettingIndex::unknown_index) {
+          // set time
+          // TODO: check if time is valid
+          // Apply time
+
+        } else {
+          if (esp3dTftsettings.isValidStringSetting(tmpstr.c_str(),
+                                                    settingIndex[i])) {
+            esp3d_log("Value %s is valid", tmpstr.c_str());
+            if (!esp3dTftsettings.writeString(settingIndex[i],
+                                              tmpstr.c_str())) {
               hasError = true;
-              error_msg = "Invalid parameter";
+              error_msg = "Set value failed";
+            }
+          } else {
+            hasError = true;
+            error_msg = "Invalid token parameter";
           }
         }
       }
-      if (!hasParam && !hasError) {
-        hasError = true;
-        error_msg = "Invalid parameter";
-      }
-      if (!hasError) {
-        // apply changes without restarting the board
-        esp3dNotificationsService.begin();
-      }
     }
-    if (!dispatchAnswer(msg, COMMAND_ID, json, hasError,
-                        hasError ? error_msg.c_str() : ok_msg.c_str())) {
-      esp3d_log_e("Error sending response to clients");
-    }*/
+    // TODO: process the now
+    // TODO: process the sync
+    if (!hasParam && !hasError) {
+      hasError = true;
+      error_msg = "Invalid parameter";
+    }
+    if (!hasError) {
+      // apply changes without restarting the board
+      esp3dTimeService.begin();
+    }
+  }
+  if (!dispatchAnswer(msg, COMMAND_ID, json, hasError,
+                      hasError ? error_msg.c_str() : ok_msg.c_str())) {
+    esp3d_log_e("Error sending response to clients");
+  }
 }
 #endif  // ESP3D_TIMESTAMP_FEATURE
