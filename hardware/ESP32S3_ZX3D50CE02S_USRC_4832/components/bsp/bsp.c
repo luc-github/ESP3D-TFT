9446f5b2454f6faaafbbc06a00191d5d8f803ea6
Luc <8822552+luc-github@users.noreply.github.com>
2024-03-24 11:15:55 +0800

    ESP32S3_ZX3D50CE02S_USRC_4832 refactoring - format bsp.c

--- hardware/ESP32S3_ZX3D50CE02S_USRC_4832/components/bsp/bsp.c
+++ hardware/ESP32S3_ZX3D50CE02S_USRC_4832/components/bsp/bsp.c
@@ -22,7 +22,9 @@
  *      INCLUDES
  *********************/
 #include "bsp.h"
+#include "esp3d_log.h"
 
+#if ESP3D_DISPLAY_FEATURE
 #include "disp_def.h"
 #include "esp3d_log.h"
 #include "ft5x06.h"
@@ -31,62 +33,82 @@
 #include "lvgl.h"
 #include "st7796.h"
 #include "touch_def.h"
+#endif  // ESP3D_DISPLAY_FEATURE
 #include "usb_serial.h"
 
-static i2c_bus_handle_t i2c_bus_handle = NULL;
-
-/*
-
-*/
-
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
 
 /**********************
  *  STATIC VARIABLES
  **********************/
+#if ESP3D_DISPLAY_FEATURE
+static i2c_bus_handle_t i2c_bus_handle = NULL;
+static lv_disp_drv_t disp_drv;
+//static esp_lcd_panel_handle_t panel_handle = NULL;
 static i2c_config_t conf = {.mode = I2C_MODE_MASTER,
                             .scl_io_num = I2C_SCL_PIN,
                             .sda_io_num = I2C_SDA_PIN,
                             .scl_pullup_en = GPIO_PULLUP_ENABLE,
                             .sda_pullup_en = GPIO_PULLUP_ENABLE,
                             .master.clk_speed = I2C_CLK_SPEED};
+#endif  // ESP3D_DISPLAY_FEATURE
+
+
 /**********************
- *      MACROS
+ *  STATIC PROTOTYPES
  **********************/
+#if ESP3D_DISPLAY_FEATURE
+//static void lv_touch_read(lv_indev_drv_t* drv, lv_indev_data_t* data);
+//void display_flush_ready();
+//void st7796_flush(lv_disp_drv_t *disp_drv, const lv_area_t *area,
+//                  lv_color_t *color_p);
+#endif // ESP3D_DISPLAY_FEATURE
+
 
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
 
+/**
+ * @brief Initializes the USB functionality of the BSP.
+ *
+ * This function initializes the USB functionality of the BSP (Board Support Package).
+ * It performs any necessary configuration and setup for USB communication.
+ *
+ * @return esp_err_t Returns ESP_OK if the USB initialization is successful, otherwise returns an error code.
+ */
 esp_err_t bsp_init_usb(void) {
   /*usb host initialization */
-  esp3d_log("Initializing usb-serial");
+  esp3d_log_d("Initializing usb-serial");
   return usb_serial_create_task();
 }
 
+/**
+ * @brief Deinitializes the USB functionality of the BSP.
+ *
+ * This function is responsible for deinitializing the USB functionality of the BSP.
+ *
+ * @return esp_err_t Returns ESP_OK if the USB deinitialization is successful, otherwise returns an error code.
+ */
 esp_err_t bsp_deinit_usb(void) {
-  esp3d_log("Remove usb-serial");
+  esp3d_log_d("Remove usb-serial");
   return usb_serial_deinit();
 }
 
+/**
+ * @brief Initializes the Board Support Package (BSP).
+ *
+ * This function initializes the necessary components and peripherals required by the BSP.
+ *
+ * @return esp_err_t Returns ESP_OK on success, or an error code if initialization fails.
+ */
 esp_err_t bsp_init(void) {
-  static lv_disp_drv_t disp_drv; /*Descriptor of a display driver*/
+  #if ESP3D_DISPLAY_FEATURE
 
   // Drivers initialization
-  esp3d_log("Display buffer size: %d", DISP_BUF_SIZE);
+  esp3d_log_d("Display buffer size: %d", DISP_BUF_SIZE);
 
   /* i2c controller initialization */
-  esp3d_log("Initializing i2C controller");
+  esp3d_log_d("Initializing i2C controller");
 
   if (NULL != i2c_bus_handle) {
     esp3d_log_e("I2C bus already initialized.");
@@ -98,30 +120,34 @@ esp_err_t bsp_init(void) {
     esp3d_log_e("I2C bus failed to be initialized.");
     return ESP_FAIL;
   }
+#endif  // ESP3D_DISPLAY_FEATURE
+
   // NOTE:
   // this location allows usb-host driver to be installed - later it will failed
   // Do not know why...
   if (usb_serial_init() != ESP_OK) {
     return ESP_FAIL;
   }
-
+  #if ESP3D_DISPLAY_FEATURE
   /* Display controller initialization */
-  esp3d_log("Initializing display controller");
+  esp3d_log_d("Initializing display controller");
   if (st7796_init(&disp_drv) != ESP_OK) {
     return ESP_FAIL;
   }
 
   /* Touch controller initialization */
-  esp3d_log("Initializing touch controller");
+  esp3d_log_d("Initializing touch controller...");
+  bool has_touch = true;
   if (ft5x06_init(i2c_bus_handle) != ESP_OK) {
-    return ESP_FAIL;
+     esp3d_log_e("Touch controller initialization failed!");
+    has_touch = false;
   }
 
   // Lvgl initialization
   lv_init();
 
   // Lvgl setup
-  esp3d_log("Setup Lvgl");
+  esp3d_log_d("Setup Lvgl");
   lv_color_t* buf1 = (lv_color_t*)heap_caps_malloc(
       DISP_BUF_SIZE * sizeof(lv_color_t), MALLOC_CAP_DMA);
   if (buf1 == NULL) {
@@ -155,12 +181,15 @@ esp_err_t bsp_init(void) {
   disp_drv.user_data = *panel_handle;
   lv_disp_drv_register(&disp_drv); /*Finally register the driver*/
 
-  /* Register an input device */
-  static lv_indev_drv_t indev_drv; /*Descriptor of a input device driver*/
-  lv_indev_drv_init(&indev_drv);   /*Basic initialization*/
-  indev_drv.type = LV_INDEV_TYPE_POINTER; /*Touch pad is a pointer-like device*/
-  indev_drv.read_cb = ft5x06_read;        /*Set your driver function*/
-  lv_indev_drv_register(&indev_drv);      /*Finally register the driver*/
-
+   if (has_touch) {
+    /* Register an input device */
+    static lv_indev_drv_t indev_drv; /*Descriptor of a input device driver*/
+    lv_indev_drv_init(&indev_drv);   /*Basic initialization*/
+    indev_drv.type =
+        LV_INDEV_TYPE_POINTER;         /*Touch pad is a pointer-like device*/
+    indev_drv.read_cb = ft5x06_read; /*Set your driver function*/
+    lv_indev_drv_register(&indev_drv); /*Finally register the driver*/
+  }
+#endif  // ESP3D_DISPLAY_FEATURE
   return ESP_OK;
 }
