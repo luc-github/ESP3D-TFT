07a1ce68d71608ea8319ac3a6e88bd3fb0dfde81
Luc <8822552+luc-github@users.noreply.github.com>
2024-04-05 18:47:50 +0800

    Fix Xiao Sense Compilation issues

--- cmake/targets/ESP32S3_SEEED_STUDIO_XIAO.cmake
+++ cmake/targets/ESP32S3_SEEED_STUDIO_XIAO.cmake
@@ -1,8 +1,8 @@
 if(ESP32S3_SEEED_STUDIO_XIAO)
     set(TFT_TARGET "ESP32S3_SEEED_STUDIO_XIAO")
-    set(SDKCONFIG ${CMAKE_SOURCE_DIR}/hardware/std::ESP32S3_SEEED_STUDIO_XIAO/sdkconfig)
-    list(APPEND EXTRA_COMPONENT_DIRS ${CMAKE_SOURCE_DIR}/hardware/std::ESP32S3_SEEED_STUDIO_XIAO/components)
-    add_compile_options("-I${CMAKE_SOURCE_DIR}/hardware/std::ESP32S3_SEEED_STUDIO_XIAO/components/bsp")    
+    set(SDKCONFIG ${CMAKE_SOURCE_DIR}/hardware/ESP32S3_SEEED_STUDIO_XIAO/sdkconfig)
+    list(APPEND EXTRA_COMPONENT_DIRS ${CMAKE_SOURCE_DIR}/hardware/ESP32S3_SEEED_STUDIO_XIAO/components)
+    add_compile_options("-I${CMAKE_SOURCE_DIR}/hardware/ESP32S3_SEEED_STUDIO_XIAO/components/bsp")    
     if (USB_SERIAL_SERVICE)
         # Enable USB-OTG as serial alternative for communications
         add_compile_options(-DESP3D_USB_SERIAL_FEATURE=1)
