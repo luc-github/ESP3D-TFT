98b95ae1ecd64e054a87914a07b63cf01cefb56a
liqijian101 <1329616343@qq.com>
2024-04-13 18:08:05 +0800

    Modify ESP32S3-BZM-TFT35_GT911 to SPI screen

--- hardware/ESP32S3_BZM_TFT35_GT911/components/bsp/touch_def.h
+++ hardware/ESP32S3_BZM_TFT35_GT911/components/bsp/touch_def.h
@@ -16,9 +16,9 @@ const gt911_config_t gt911_cfg = {
             0x5D, 0X14,
             0},  // Floating or mis-configured INT pin may cause GT911 to come
                  // up at address 0x14 instead of 0x5D, so check there as well.
-    .rst_pin = -1, // GPIO3 but already setup by display driver so not needed here
+    .rst_pin = -1, // GPIO25 but already setup by display driver so not needed here
 #if WITH_GT911_INT
-    .int_pin = 38, // GPIO 38
+    .int_pin = 1, // GPIO 1
 #else
     .int_pin = -1, // INT pin not connected (by default)
 #endif
