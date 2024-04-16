81692fd40bd1c20237d9f1bd3294ea993c5a54dd
Luc <8822552+luc-github@users.noreply.github.com>
2024-04-16 16:51:33 +0800

    Add rotrix to ci
    change default config of freenove

--- .github/ci/ESP32S3_FREENOVE.sh
+++ .github/ci/ESP32S3_FREENOVE.sh
@@ -7,6 +7,8 @@ cd $GITHUB_WORKSPACE
 idf.py fullclean
 sed -i '1,32s/ ON/ OFF/g' ./CMakeLists.txt
 sed -i '1,32s/OPTION(ESP32S3_FREENOVE_1_1 "HARDWARE TARGET is ESP32S3 Freenove v1.1" OFF/OPTION(ESP32S3_FREENOVE_1_1 "HARDWARE TARGET is ESP32S3 Freenove v1.1" ON/g' ./CMakeLists.txt
+sed -i 'OPTION(CAMERA_SERVICE "Camera service" OFF/OPTION(CAMERA_SERVICE "Camera service" ON/g' ./CMakeLists.txt
+sed -i 'OPTION(TFT_UI_SERVICE "TFT UI service" ON/OPTION(TFT_UI_SERVICE "TFT UI service" OFF/g' ./CMakeLists.txt
 idf.py fullclean
 head ./CMakeLists.txt -n 9
 idf.py -DIDF_TARGET=esp32s3 reconfigure
