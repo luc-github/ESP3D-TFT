f68290bef87688b5c306e76d4c130b9330016385
Luc <8822552+luc-github@users.noreply.github.com>
2024-08-14 11:55:00 +0800

    Update CMakeLists.txt to revert default value
    Update scripts to take new line in account when doing setting

--- .github/ci/ESP32S3_8048S070C.sh
+++ .github/ci/ESP32S3_8048S070C.sh
@@ -5,8 +5,8 @@ cd $GITHUB_WORKSPACE/esp-idf
 source ./export.sh
 cd $GITHUB_WORKSPACE
 idf.py fullclean
-sed -i '1,32s/ ON/ OFF/g' ./CMakeLists.txt
-sed -i '1,32s/ESP32S3_8048S070C "TFT TARGET is ESP32S3_8048S070C - 7.0in. 800x480 (Capacitive)" OFF/ESP32S3_8048S070C "TFT TARGET is ESP32S3_8048S070C - 7.0in. 800x480 (Capacitive)" ON/g' ./CMakeLists.txt
+sed -i '1,36s/ ON/ OFF/g' ./CMakeLists.txt
+sed -i '1,36s/ESP32S3_8048S070C "TFT TARGET is ESP32S3_8048S070C - 7.0in. 800x480 (Capacitive)" OFF/ESP32S3_8048S070C "TFT TARGET is ESP32S3_8048S070C - 7.0in. 800x480 (Capacitive)" ON/g' ./CMakeLists.txt
 idf.py fullclean
 head ./CMakeLists.txt -n 9
 idf.py -DIDF_TARGET=esp32s3 reconfigure
--- .github/ci/ZX3D50CE02S-SRC-4832.sh
+++ .github/ci/ZX3D50CE02S-SRC-4832.sh
@@ -5,8 +5,8 @@ cd $GITHUB_WORKSPACE/esp-idf
 source ./export.sh
 cd $GITHUB_WORKSPACE
 idf.py fullclean
-sed -i '1,32s/ ON/ OFF/g' ./CMakeLists.txt
-sed -i '1,32s/ESP32S3_ZX3D50CE02S_USRC_4832 "TFT TARGET is ESP32S3 Panlee ZX3D50CE02S-SRC-4832 3.5inches" OFF/ESP32S3_ZX3D50CE02S_USRC_4832 "TFT TARGET is ESP32S3 Panlee ZX3D50CE02S-SRC-4832 3.5inches" ON/g' ./CMakeLists.txt
+sed -i '1,36s/ ON/ OFF/g' ./CMakeLists.txt
+sed -i '1,36s/ESP32S3_ZX3D50CE02S_USRC_4832 "TFT TARGET is ESP32S3 Panlee ZX3D50CE02S-SRC-4832 3.5inches" OFF/ESP32S3_ZX3D50CE02S_USRC_4832 "TFT TARGET is ESP32S3 Panlee ZX3D50CE02S-SRC-4832 3.5inches" ON/g' ./CMakeLists.txt
 idf.py fullclean
 head ./CMakeLists.txt -n 9
 idf.py -DIDF_TARGET=esp32s3 reconfigure
